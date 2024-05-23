bbe810be49092f548b9487ba3867b9d48fd23bae
Drew Merrill <drewcmerrill@gmail.com>
2024-05-23 11:17:44 -0700

    Bento Grid complete

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,5 +1,14 @@
-import React, { ReactNode } from "react";
+import React from "react";
+import { div } from "three/examples/jsm/nodes/Nodes.js";
 
+/**
+ *  UI: border magic from tailwind css btns
+ *  Link: https://ui.aceternity.com/components/tailwindcss-buttons
+ *
+ *  change border radius to rounded-lg
+ *  add margin of md:mt-10
+ *  remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
+ */
 const MagicButton = ({
   title,
   icon,
@@ -14,16 +23,24 @@ const MagicButton = ({
   otherClasses?: string;
 }) => {
   return (
-    <button className="relative inline-flex w-full h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 md:w-60 md:mt-10">
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-      <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-white px-7 text-sm font-medium text-slate backdrop-blur-3xl gap-2 ${otherClasses}`}
+    <div>
+      <button
+        className="relative inline-flex rounded-full h-12 w-full md:w-60 md:mt-10 overflow-hidden  p-[2px] focus:outline-none"
+        onClick={handleClick}
       >
-        {position === "left" && icon}
-        {title}
-        {position === "right" && icon}
-      </span>
-    </button>
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+
+        {/* remove px-3 py-1, add px-5 gap-2 */}
+        <span
+          className={`inline-flex h-full w-full rounded-full cursor-pointer items-center justify-center
+             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
+        >
+          {position === "left" && icon}
+          {title}
+          {position === "right" && icon}
+        </span>
+      </button>
+    </div>
   );
 };
 
